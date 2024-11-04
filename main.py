41e4e24d508beb8cfbfa920bc5f86b3372cc958e
Shayan Mokhtary <61922576+mahdimmm138031@users.noreply.github.com>
2024-11-04 04:22:12 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import googletrans
+import discord
+import os
+import webserver
+
+def translate_text(input_text, target_language='en'):
+    translator = googletrans.Translator()
+    translated = translator.translate(input_text, dest=target_language)
+    return translated.text
+
+# Get user input
+class Client(discord.Client):
+
+    async def on_message(self, message):
+        if message.content.startswith('-en'):
+            target_lang = 'en' 
+            user_input = message.content[4:]
+            translated_text = translate_text(user_input, target_lang)
+            await message.channel.send(translated_text, reference=message)
+        elif message.content.startswith('-ch'):
+            target_lang = 'zh-cn' 
+            user_input = message.content[4:]
+            translated_text = translate_text(user_input, target_lang)
+            await message.channel.send(translated_text, reference=message)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(os.environ['discordkey'])
+webserver.keep_alive()
